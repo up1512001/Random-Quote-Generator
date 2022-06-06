2b66744e695a75df578ca1f7a7ea0ead75fef257
up1512001 <utsav.patel1215@outlook.com>
2022-06-06 23:22:05 +0530

    some css improvements left

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+import './App.css';
+
+import axios from 'axios';
+
+class App extends React.Component {
+
+    state = {
+        advice : ''
+    };
+
+
+    componentDidMount(){
+        this.fetchAdvice();
+    }
+
+    fetchAdvice = () =>{
+        axios.get('https://api.adviceslip.com/advice')
+        .then((responce)=>{
+            const {advice} = responce.data.slip;
+            
+            this.setState({ advice : advice });
+            
+        }).catch((error)=>{
+            console.log(error);
+        });
+    }
+
+    render(){
+
+        const { advice } = this.state;
+
+        return (
+            <div className="app">
+                <div className="card">
+                    <h1 className="heading">{advice}</h1>
+                    <button className="button" onClick={this.fetchAdvice}>
+                        <span>GIVE ME ADVICE !!!</span>
+                    </button>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+export default App;
